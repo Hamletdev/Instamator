109b01bcc3ab3128234dedb6aada90f329debc84
Hamletdev <hamletdev001@gmail.com>
2020-12-10 02:12:56 +0530

    Initial Commit

---
+++ Instamator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instamator
+//
+//  Created by Amit Chaudhary on 12/10/20.
+//  Copyright © 2020 Amit Chaudhary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
